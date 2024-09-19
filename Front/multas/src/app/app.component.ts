ec153bbeae52738914b54428d4740c335d830b08
Sarate Luciana <lusarate@gmail.com>
2024-09-18 23:57:18 -0300

    Actualización de los cambios

--- Front/multas/src/app/app.component.ts
+++ Front/multas/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListaDenunciasComponent } from "./lista-denuncias/lista-denuncias.component";
+import { NuevaDenunciaComponent } from "./nueva-denuncia/nueva-denuncia.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListaDenunciasComponent],
+  imports: [RouterOutlet, ListaDenunciasComponent, NuevaDenunciaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
